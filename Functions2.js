d9289306c225da8267344a8072643d1ed4c99baf
DariaIvanita <147073195+DariaIvanita@users.noreply.github.com>
2024-06-21 11:24:08 -0600

    Add files via upload

---
+++ Functions2.js
@@ -0,0 +1,13 @@
+//dark.addEventLister("click",function(){
+    //document.body.style.backgroundColor="black";
+    //document.body.style.color="white"
+    //});
+dark.addEventLister("click),function(){
+    darkMode();
+    // call the function for lightmode
+});
+function darkMode(){
+    document.body.style.backgroundColor="Black";
+    document.body.style.color="white";
+}
+//function for lightmode
